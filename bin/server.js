4afd6a6702b6142376741fe997bc3850320b3545
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-23 10:44:34 +0200

    Create first template with use property name with handlebars

--- bin/server.js
+++ bin/server.js
@@ -13,7 +13,7 @@ var express = require( "express" ),
 // setup express
 oApp = express();
 
-
+// Configuration middlewares
 oApp.use( responseTime() ); // Ça rajoute un header response-time qui mesure le temps de réponse du serveur (voir onglet network)
 oApp.use ( bodyParser.urlencoded( {
   "extended": true
@@ -21,11 +21,17 @@ oApp.use ( bodyParser.urlencoded( {
 oApp.use( bodyParser.json() );
 oApp.use( logMiddleWare );
 
+// Configure template engine
+oApp.set( "views", __dirname + "/views" ); // On crée dans /bin un dossier /views
+oApp.set ( "view engine", "hbs" );
 
+// Configuration routes
+oApp.get( "/", function( oRequest, oResponse ) { // oApp c'est express. On lui dit que la route d'entrée (en GET), la function que l'on a définit sera exécutée
+  oResponse.render( "index.hbs", {
+    "name": "Jimmy"
+  } );
+})
 
-oApp.get( "/", function( oRequest, oResponse ){ // oApp c'est express. On lui dit que la route d'entrée (en GET), la function que l'on a définit sera exécutée
-  oResponse.send( "Hello, world!" );
-} );
 
 oApp.listen( 8080, function(){ // La function est exécutée si tout se passe bien.
   console.log( "Express is listening on port 8080" );
