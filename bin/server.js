b28fc3bb0c5ac585e1309f5c940a68e19c0a90ca
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-23 10:16:36 +0200

    Initialise express server

---
+++ bin/server.js
@@ -0,0 +1,24 @@
+/* jimmylet/meet-express
+* bin/server.js
+* started at 23/09/2016
+*/
+"user strict";
+
+var express = require( "express" ),
+    logMiddleWare = require( "./middlewares/log" ), // on peut ne pas mettre le .js à log
+    oApp;
+
+// setup express
+oApp = express();
+
+oApp.use( logMiddleWare );
+
+
+
+oApp.get( "/", function( oRequest, oResponse ){ // oApp c'est express. On lui dit que la route d'entrée (en GET), la function que l'on a définit sera exécutée
+  oResponse.send( "Hello, world!" );
+} );
+
+oApp.listen( 8080, function(){ // La function est exécutée si tout se passe bien.
+  console.log( "Express is listening on port 8080" );
+} );
